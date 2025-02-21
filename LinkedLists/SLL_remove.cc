d733785768be07c347f16c42f51d9493759eacf5
Patricia Lindner <lindnerp@ohio.edu>
2025-02-21 15:25:57 -0500

    Adding singly linked list examples from class.

---
+++ LinkedLists/SLL_remove.cc
@@ -0,0 +1,140 @@
+/**
+ * @brief This file gives example code from class for using a linked list as part of a main program
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+};
+
+/**
+ * @brief output all the data in the list
+ * 
+ * @param head - pointer to the beginning of the list
+ */
+void show_all(node* head){
+    node* tmp = head;
+    while(tmp != nullptr){
+        cout << tmp -> data << endl;
+        tmp = tmp -> next;
+    }
+}
+
+/**
+ * @brief add a new node to the beginning of the list with the data in item
+ * 
+ * @param head - pointer to the beginning of the list
+ * @param item - the data I want to add to the new node
+ */
+void add_to_beginning(node*& head, int item){
+    if(head == nullptr){
+        head = new node;
+        head -> data = item;
+        head -> next = nullptr;
+        return;
+    }
+    else{
+        node* tmp = new node;
+        tmp -> data = item;
+        tmp -> next = head;
+        head = tmp;
+    }
+}
+
+/**
+ * @brief add a new node with the data in item to the end of the list
+ * 
+ * @param head - pointer to the beginning of the list
+ * @param item - the data for the new node
+ */
+void add_to_end(node*& head, int item){
+    if(head == nullptr){
+        head = new node;
+        head -> data = item;
+        head -> next = nullptr;
+        return;
+    }
+    
+    node* tmp = head;
+    while(tmp -> next != nullptr){
+        tmp = tmp -> next;
+    }
+    tmp -> next = new node;
+    tmp -> next -> data = item;
+    tmp -> next -> next = nullptr;
+}
+
+/**
+ * @brief remove the node with the target data from the list
+ * 
+ * @param head - pointer to the beginning of the list
+ * @param target - the item I want to remove
+ */
+void remove(node*& head, int target){
+    // handle empty list
+    if (head == nullptr) return;
+
+    // move tmp to point at the node before the one I want to remove
+    node *before, *tmp;
+    for(tmp = head; tmp -> next != nullptr && tmp -> next -> data != target; tmp = tmp -> next);
+
+    // since we could have exited the loop without finding the data we were looking for
+    // check to make sure that if tmp is pointing to the last node, the data isn't what we were wanting to remove
+    // if both are true (tmp pointing to last node AND data in the last node is not the target)
+    // then exit the function because the data isn't in the list
+    if(tmp -> next == nullptr && tmp -> data != target) return;
+
+    // if we make it here, then tmp is pointing to the node before the one we want to remove
+    // set before pointer to tmp and then move tmp to next node
+    before = tmp;
+    tmp = tmp -> next;
+
+    // make the node before the one we want to remove point to the node after the one we want to remove
+    before -> next = tmp -> next;
+    // deallocate the node we wanted to remove
+    delete tmp;
+}
+
+/* 
+
+Special cases: 
+1. item not found
+2. empty list
+3. last element
+
+4. one item
+5. first element
+
+*/
+
+int main(){
+    // start with an empty list
+    node* head = nullptr;
+
+    for(int i = 1; i < 5; i++)
+        add_to_beginning(head, i);
+    
+    cout << "List after add_to_beginning:\n";
+    show_all(head);
+
+    for(int i = 5; i < 10; i++)
+        add_to_end(head, i);
+
+    cout << "List after add_to_end:\n";
+    show_all(head);
+
+    // remove a node in the middle of the list
+    remove(head, 1);
+    cout << "List after removing 1:\n";
+    show_all(head);
+
+    // remove the last node (9)
+    remove(head, 9);
+    cout << "List after removing last node:\n";
+    show_all(head);
+
+    // Not complete, not all special cases implemented in the remove function yet
+}
