212892457abfbd6ef691a6df6b98304ee382d86c
Patricia Lindner <lindnerp@ohio.edu>
2026-01-14 11:43:49 -0500

    Adding Bankacct header file.

---
+++ Review/bank.h
@@ -0,0 +1,15 @@
+#include <string>
+
+#ifndef BANK_H
+#define BANK_H 
+
+class Bankacct{
+
+
+    private:
+        std::string owner;
+        double balance;
+        double interest_r;
+};
+
+#endif
