0d41a53b827ad8cd7d41ae373549097a4e2bdd8b
Patricia Lindner <lindnerp@ohio.edu>
2026-02-25 10:57:38 -0500

    Updating LL class notes

--- LinkedLists/LL.cc
+++ LinkedLists/LL.cc
@@ -44,17 +44,17 @@ LL_Container::LL_Container(const LL_Container& other){
 
 
     if(other.head == nullptr){
-        head = nullptr;
+        head = tail = nullptr;
     }
     else{
-        NODE* src, *dest;
+        NODE* src;
         head = new NODE(other.head -> data());
         src = other.head -> next();
-        dest = head;
+        tail = head;
         while(src != nullptr){
-            dest -> set_next(new NODE(src -> data()));
-            //dest -> next() = new NODE(src -> data());
-            dest = dest -> next();
+            tail -> set_next(new NODE(src -> data()));
+            //tail -> next() = new NODE(src -> data());
+            tail = tail -> next();
             src = src -> next();
         }
     }
@@ -93,4 +93,97 @@ LL_Container& LL_Container::operator = (const LL_Container& other){
     }
 
     return *this;
+}
+
+void LL_Container::add_to_end(int item){
+    /*
+    // no tail version
+    if(head == nullptr){
+        head = new NODE(item);
+        return;
+    }
+    NODE* cursor;
+    for(cursor = head; cursor -> next() != nullptr; cursor = cursor -> next()); 
+
+    cursor -> set_next(new NODE(item));
+    //(*cursor).set_next(new NODE(item));
+    */
+
+    // tail version
+    if(head == nullptr){
+        head = tail = new NODE(item);
+        return;
+    }
+    tail -> set_next(new NODE(item));
+    tail = tail -> next();
+}
+
+void LL_Container::add_to_head(int item){
+    // no tail version
+    //head = new NODE(item, head);
+
+    // tail version
+    if(head == nullptr){
+        head = tail = new NODE(item, head);
+        return;
+    }
+    head = new NODE(item, head);
+
+
+    // tail version #2
+    head = new NODE(item, head);
+    if(tail == nullptr) tail = head;
+}
+
+void LL_Container::remove(int target){
+    /*
+        Cases we may want to consider for removal: 
+        1. empty list
+        2. one node
+        3. generic middle node
+        4. target doesn't exist
+        5. last node
+        6. first node
+        7. multiple nodes with target (optional)
+    */
+
+    // empty list
+    if(head == nullptr){
+        return;
+    }
+
+    // single node case
+    if(head -> next() == nullptr){
+        if(head -> data() == target){
+            delete head;
+            head = tail = nullptr;
+        }
+        return;
+    }
+
+    // first node
+    if(head -> data() == target){
+        NODE* second_node = head -> next();
+        delete head;
+        head = second_node;
+        return;
+    }
+
+    // generic case W/ last node and item not found
+    // THIS SEGMENT HAS NOT BEEN FULLY EDITED YET FOR CLASS NODE AND TAIL POINTER
+    NODE* cursor = head;
+    NODE* prev = nullptr;
+    for(; cursor != nullptr && (cursor -> data() != target); cursor = cursor -> next()){
+        if(cursor -> next() != nullptr && cursor -> next() -> data() == target){
+            prev = cursor;
+        }
+    }
+    if(prev != nullptr) {
+        prev -> next() = cursor -> next();
+        // removing last node
+        if(cursor == tail){
+            tail = prev;
+        }
+        delete cursor;
+    }
 }
--- LinkedLists/LL.h
+++ LinkedLists/LL.h
@@ -2,13 +2,20 @@
 
 class LL_Container{
     public:
-        LL_Container() {head = nullptr;}
+        LL_Container() {head = tail = nullptr;}
 
         // The Big 3
         ~LL_Container();
         LL_Container(const LL_Container& other);
         LL_Container& operator = (const LL_Container& other);
 
+
+        // other useful stuff
+        void add_to_end(int item);
+        void add_to_head(int item);
+        void remove(int target);
+
     private:
         NODE* head;
+        NODE* tail;
 };
