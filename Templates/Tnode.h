57f6a542dab7f22184070d6581f110e47333b363
Patricia Lindner <lindnerp@ohio.edu>
2026-03-04 10:20:43 -0500

    Finished non-member template functions. Adding beginning of template node for singly linked list.

---
+++ Templates/Tnode.h
@@ -0,0 +1,50 @@
+/**
+ * @file Tnode.h
+ * @author Patricia Lindner
+ * @brief This file provides a template framework for a singly linked list node
+ * @date 2026-03-04
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+
+/*
+    TEMPLATE RULES:
+        1. You must add the template header with template parameter before the class declaration.
+        2. Every function of a template class is a template function regardless of if it uses the template parameter type or not.
+        3. The name of a template class MUST be followed by < > every time it is used unless it is the name of a function (constructor/deconstructor).
+            a. If you are writing function definitions you use <T> (or whatever the template parameter is)
+            b. If you are writing an application (main program) you use <int> (or whatever real type you want to store in the container)
+
+*/
+
+#ifndef TNODE_H
+#define TNODE_H
+
+template <typename T>
+class Tnode{
+    public:
+        Tnode(T d = T(), Tnode* n = nullptr);
+
+        // accessors
+        Tnode* next() const;
+        T data() const {return dataF;}
+
+    private:
+        T dataF;
+        Tnode* nextF;
+};
+
+template <typename T>
+Tnode<T>* Tnode<T>::next() const{
+    return nextF;
+}
+
+template <typename T>
+Tnode<T>::Tnode(T d, Tnode<T>* n){
+    dataF = d;
+    nextF = n;
+}
+
+#endif
--- Templates/non_member.cc
+++ Templates/non_member.cc
@@ -1,20 +1,51 @@
-// template function
+/**
+ * @file non_member.cc
+ * @author Patricia Lindner
+ * @brief This file demonstrates how to write and instantiate non-member ("normal") template functions
+ * @date 2026-03-04
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #include <string>
 
-template<typename T, typename S> void my_swap(T& a, S& b);
+template<typename T> void my_swap(T& a, T& b);
+template<class T, class S> void ssort(T a[], S used);
 
 int main(){
     int x = 5, y = 7;
     std::string a = "hello", b = "world";
     char c = 'C', s = 'S';
 
+    double array[5];
+    int used = 3;
+
     my_swap(x, y); // instantiation for int
     my_swap(a, b); // instantiation for string
+
+    // the nex line doesn't compile - why?
     my_swap(x, c);
+
+    ssort(array, used); // instantiation for array of double and int used 
 }
 
-template<typename T, typename S> void my_swap(T& a, S& b){
+template<typename T> void my_swap(T& a, T& b){
     T tmp = a;
     a = b;
     b = tmp;
+}
+
+// this function will sometimes give an "unable to instantiate" error - why?
+template<class T, class S>
+void ssort(T a[], S used){
+    S smallsp;
+    for(S i = 0; i < used - 1; i++){
+        smallsp = i;
+        for(S j = i + 1; j < used; j++){
+            if(a[j] < a[smallsp]){
+                smallsp = j;
+            }
+        }
+        my_swap(a[i], s[smallsp]); // this is my template swap function
+    }
 }
