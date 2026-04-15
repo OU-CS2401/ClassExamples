50e8286df0cd424855c8b833c8de1797ba1d6a3b
Patricia Lindner <lindnerp@ohio.edu>
2026-04-15 10:14:04 -0400

    Adding size and output functions for binary trees

--- BinaryTrees/bnode_app.cc
+++ BinaryTrees/bnode_app.cc
@@ -1,4 +1,5 @@
 #include "bnode.h"
+#include <iostream>
 
 using namespace std;
 
@@ -22,5 +23,61 @@ void clear(BNODE*& r){
         clear(r -> right());
         delete r;
         r = nullptr;
+    }
+}
+
+
+int size(BNODE* root){
+    // int count = 0;
+    // base case
+    if(root == nullptr){
+        return 0;
+    }
+    else{
+        // count += size(root -> left());
+        // count += size(root -> right());
+        return 1 + size(root -> right()) + size(root -> left());
+    }
+}
+
+
+// tree traversals 
+
+// in-order: left, root, right
+void print_in_order(BNODE* root){
+    // base case
+    if(root == nullptr){
+        return;
+    }
+    else{
+        print_in_order(root -> left());
+        cout << root -> data() << endl;
+        print_in_order(root -> right());
+    }
+}
+
+// pre-order: root, left, right
+void print_pre_order(BNODE* root){
+    // base case
+    if(root == nullptr){
+        return;
+    }
+    else{
+        cout << root -> data() << endl;
+        print_pre_order(root -> left());
+        print_pre_order(root -> right());
+    }
+}
+
+// post-order: left, right, root
+void print_post_order(BNODE* root){
+    // base case
+    if(root == nullptr){
+        return;
+    }
+    else{
+        print_post_order(root -> left());
+        print_post_order(root -> right());
+        cout << root -> data() << endl;
     }
 }
